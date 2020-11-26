2232014d1d5f055668d870b8fcd4d239528da343
Monika <mon.fur89@gmail.com>
2020-11-27 00:25:26 +0100

    feladatok

---
+++ Numbers/js/numbers1.js
@@ -0,0 +1,4 @@
+//Írj egy függvényt, ami a paraméterként kapott tetszőleges darabszámú, tetszőleges méretű egész számot összead, és visszatér az összeadás végeredményével! 
+//A paraméterként kapott értékek egyszerű number típusúak legyenek! Amennyiben bármelyik paraméter értéke, vagy a részeredmény meghaladja a biztonságos tartományt,
+//automatikusan konvertáljad BigIntbe, és így számoljunk tovább, és természetesen a visszatérési érték is BigInt legyen!
+
